1ac320b8d4baa674056b8d1d6c876075d2749dfb
hanchaoxi <hanchaoxi@sensetime.com>
2022-12-14 18:26:36 +0800

    2022-12-13笔记

--- docs/pages/essay/README.md
+++ docs/pages/essay/README.md
@@ -4,7 +4,7 @@ title: 随笔目录
 
 [[toc]]
 
-[mermaid插件使用](/pages/essay/mermaid-usage)
+[mermaid插件使用666](/pages/essay/mermaid-usage)
 
 ::: tip
 工作、生活中随想笔记
--- docs/pages/essay/sidebar_contents.js
+++ docs/pages/essay/sidebar_contents.js
@@ -1,4 +1,11 @@
+const noteModules = []
+require('fs').readdirSync(__dirname).forEach(function(file) {
+	if (file.match(/\.md$/) !== null && file !== 'README.md') {
+		const name = file.replace('.md', '');
+		noteModules.push(name);
+	}
+});
 module.exports = [
 	"/pages/essay/",
-	"mermaid-usage"
+	...noteModules
 ];
